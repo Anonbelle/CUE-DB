80a142be510d88966a74b33564bb04b45a61f884
Anonbelle <57560995+Anonbelle@users.noreply.github.com>
2019-11-11 21:51:34 +0200

    v.1.0 Update Test

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # CUE-Test
 
 Little CUE database made just cause.
+
+https://anonbelle.github.io/CUE-DB/
+
+v.1.0
++ Design updated;
++ Recording missions (useless thing) and degree is added.
+
---
+++ scripts/recordingmissions.js
@@ -0,0 +1,43 @@
+
+    function fill(number, width) {
+        var numberOutput = Math.abs(number);
+        var length = number.toString().length;
+        var zero = "0";
+        if (width <= length) {
+            if (number < 0) {
+                return ("-" + numberOutput.toString());
+            } else {
+                return numberOutput.toString();
+            }
+        } else {
+            if (number < 0) {
+                return ("-" + (zero.repeat(width - length)) + numberOutput.toString());
+            } else {
+                return ((zero.repeat(width - length)) + numberOutput.toString());
+            }
+        }
+    }
+
+    $(document).ready(function () {
+        $.getJSON("https://anonbelle.github.io/CUE-DB/json/RecordingMissionMaster.json", function (data) {
+            var chardata = '';
+            var x = 0;
+            $.each(data, function (key, value) {
+                chardata += '<tr>';
+                chardata += '<td>' + value.name + '</td>';
+                chardata += '<td>' + value.description + '</td>';
+                chardata += '<td>' + value.showingId + '</td>';
+                chardata += '<td>' + value.chapterId + '</td>';
+                chardata += '<td>' + value.defficultyLevel + '</td>';
+                chardata += '<td name=' + 'image' + x + '>' + '<img src=' + 'https://anonbelle.github.io/CUE-DB/images/itemicon/item_' + fill(value.rewardItemType, 2) + '_' + fill(value.rewardItemId, 5) + '.png' + '>' + '</td>';
+                chardata += '<td>' + value.rewardItemQuantity + '</td>';
+                chardata += '</tr>';
+                x = x + 1;
+            });
+            $('#ChTable').append(chardata);
+            $('tbody').paginathing({
+                perPage: 15,
+                limitPagination: 5,
+            });
+        });
+    });
